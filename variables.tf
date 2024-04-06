00f589fa2cc305a92825e8d3d6f596597e9ea84f
HakimHC <ahmedh90111@gmail.com>
2024-04-06 19:33:36 +0200

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "repo_name" {
+  type = string
+  description = "Name of the github repository configured in Cloud Build."
+}
+
+variable "github_owner" {
+  type = string
+  description = "Owner of the github repository."
+}
