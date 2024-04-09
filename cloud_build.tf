0d30edbeb92e9da6676c2d351f9737ef642433a8
HakimHC <ahmedh90111@gmail.com>
2024-04-09 13:04:29 +0200

    Updated cloud build trigger (sa && more env vars)

--- cloud_build.tf
+++ cloud_build.tf
@@ -1,10 +1,10 @@
 resource "google_cloudbuild_trigger" "offline-wp-trigger" {
   location = var.region
-  name = "offline-wp-trigger"
+  name     = "offline-wp-trigger"
 
   github {
     owner = var.github_owner
-    name = var.repo_name
+    name  = var.repo_name
 
     push {
       branch = "^main$"
@@ -12,10 +12,15 @@ resource "google_cloudbuild_trigger" "offline-wp-trigger" {
   }
 
   substitutions = {
-    _LOCATION = var.region
+    _LOCATION   = var.region
     _PROJECT_ID = var.project
     _REPOSITORY = google_artifact_registry_repository.wp-repo.name
+    _PIPELINE   = google_clouddeploy_delivery_pipeline.primary.name
+    _VPC        = google_vpc_access_connector.vpc_connector.name
   }
+
+  service_account = google_service_account.cloud_build_account.id
+
 
   filename = var.cloud_build_yaml_path
 }
